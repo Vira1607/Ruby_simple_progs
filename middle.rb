b8ccded6186dcc2c112a280691ef873fe47a4355
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-18 00:32:20 +0200

    Create middle.rb

---
+++ middle.rb
@@ -0,0 +1,12 @@
+# C:\Ruby\lesson_17\middle.rb
+
+puts "Введите число 1:"
+ch1 = gets.to_i
+puts "Введите число 2:"
+ch2 = gets.to_i
+puts "Введите число 3:"
+ch3 = gets.to_i
+
+average = (ch1 + ch2 + ch3) / 3
+
+puts "Среднее арифметическое:" + average.to_s
