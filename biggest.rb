ead25c6bc0bef14e4134822c52f998e6184024c7
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-17 23:10:59 +0200

    Create biggest.rb

---
+++ biggest.rb
@@ -0,0 +1,27 @@
+ch = 172.169
+
+ch2 = nil
+ch2 = 31.514
+
+puts "Наибольшее число:"
+
+if (ch > ch2)
+	puts ch
+else 
+	puts ch2
+end
+
+#C:\Ruby\lesson_10\biggest.rb
+
+
+ch = 172.169
+
+ch2 = nil
+ch2 = 31.514
+
+
+if (ch > ch2)
+	puts "Наибольшее число:" + ch.to_s
+else 
+	puts "Наибольшее число:" + ch2.to_s
+end
