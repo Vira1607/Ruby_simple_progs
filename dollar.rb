9e7d1ce8f12d37c22f3c161b92e3a0e8ccfb04cc
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-18 00:42:44 +0200

    Create dollar.rb

---
+++ dollar.rb
@@ -0,0 +1,10 @@
+# C:\Ruby\lesson_18\dollar.rb
+
+puts "Сколько сейчас стоит 1 доллар в рублях?"
+ch1 = gets.to_i
+puts "Сколько  у вас рублей?"
+ch2 = gets.to_i
+
+average = ch1 * ch2 
+
+puts "Среднее арифметическое:" + average.to_s
