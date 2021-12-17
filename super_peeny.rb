ef19816eced86d77be9902c65b3d3dfc3308579a
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-18 00:30:31 +0200

    Create super_peeny.rb

---
+++ super_peeny.rb
@@ -0,0 +1,13 @@
+# C:\Ruby\lesson_15\super_peeny.rb
+
+m = rand (0..10)
+
+if m >= 6 
+	puts "Выпала решка"
+else
+	if m == 0
+		puts "Монета встала на ребро"
+	else 
+		puts "Выпал орёл"
+	end
+end
