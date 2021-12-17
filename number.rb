3b597806bdd7e9da1b6b031d05991eecce2dafbe
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-17 23:07:43 +0200

    Create number.rb

---
+++ number.rb
@@ -0,0 +1,9 @@
+number = 2308
+number2 = 13
+average = (number + number2) / 2
+
+puts 'Первое число:' + number.to_s
+puts 'Второе число:' + number2.to_s
+puts 'Среднее:' + average.to_s
+
+#C:\Ruby\lesson_11\number.rb
