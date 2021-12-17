44e8f679ea06d13df43031078126c15f0a7331d1
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-18 00:46:00 +0200

    Create dollar-rub.rb

---
+++ dollar-rub.rb
@@ -0,0 +1,10 @@
+# C:\Ruby\lesson_19\dollar-rub.rb
+
+puts "Сколько сейчас стоит 1 доллар в рублях?"
+ch1 = gets.to_i
+puts "Сколько  у вас рублей?"
+ch2 = gets.to_i
+
+average = ch1 * ch2 
+
+puts "Ваши запасы равны: $ " + average.to_s
